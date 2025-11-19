ec3a003342860aa03dacf1488ef0af15b1719024
Shehriyar Nadeem <shehriyar.nadeem@10pearls>
2025-11-19 19:34:28 +0500

    push

--- src/jobs/utils.ts
+++
@@ -1,6 +0,0 @@
-export function formattedBids(bids: any): any {
-  return Object.entries(bids || {}).map(([id, bid]) => ({
-    id,
-    ...(typeof bid === "object" && bid !== null ? bid : {}),
-  }));
-}
